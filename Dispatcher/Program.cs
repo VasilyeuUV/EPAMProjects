a72dce7bce5ae685a23923af67454497fa1e2f4d
Uladzislau Vasilyeu <vasilyeu.uv@mail.ru>
2019-12-31 06:59:02 +0300

    parser: tmp vertion

---
+++ Dispatcher/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileParcer
+{
+    class Program
+    {
+        string fileCSV = @"D:\Ivanov_20.12.2019_1.csv";
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
